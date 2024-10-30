7e38b773b7a1ce57d994af07e9cb4a96ba1644e5
saloum45 <saloumfall45@gmail.com>
2024-10-30 20:43:38 +0000

    in progress

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -1,26 +1,39 @@
 <?php
 
-namespace dynamic-laravel;
+namespace controllergenerate;
 
 use Illuminate\Support\ServiceProvider;
-use saloum45\dynamic-laravel\src\Commands\GenerateControllers;
+use Illuminate\Support\Facades\File;
 
 class PackageServiceProvider extends ServiceProvider
 {
     public function register()
     {
         // Enregistrer la commande
-        $this->app->singleton(GenerateControllers::class, function () {
-            return new GenerateControllers();
+        $this->app->singleton(\App\Console\Commands\GenerateControllers::class, function () {
+            return new \App\Console\Commands\GenerateControllers();
         });
 
         $this->commands([
-            GenerateControllers::class,
+            \App\Console\Commands\GenerateControllers::class,
         ]);
     }
 
     public function boot()
     {
-        //
+        // Chemin source de la commande dans le package
+        $source = __DIR__.'/Commands/GenerateControllers.php';
+
+        // Chemin de destination pour la commande dans le dossier app/Console/Commands
+        $destination = app_path('Console/Commands/GenerateControllers.php');
+
+        // Si le fichier de commande n'existe pas, le créer et y copier le contenu du fichier source
+        if (!File::exists($destination)) {
+            File::ensureDirectoryExists(app_path('Console/Commands'));
+
+            // Lire le contenu de la commande source et l'écrire dans la destination
+            $commandContent = File::get($source);
+            File::put($destination, $commandContent);
+        }
     }
 }
