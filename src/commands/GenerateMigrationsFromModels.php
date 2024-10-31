c4087eb9c9881a75a8e8eebbbd152b37f5c0e73a
saloum45 <saloumfall45@gmail.com>
2024-10-31 10:50:12 +0000

    in progress

--- src/commands/GenerateMigrationsFromModels.php
+++ src/commands/GenerateMigrationsFromModels.php
@@ -63,31 +63,31 @@ class GenerateMigrationsFromModels extends Command
         }
     
         $migrationContent = <<<EOT
-    <?php
-    
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Support\Facades\Schema;
-    
-    class Create{$tableName}Table extends Migration
-    {
-        public function up()
+        <?php
+        
+        use Illuminate\Database\Migrations\Migration;
+        use Illuminate\Database\Schema\Blueprint;
+        use Illuminate\Support\Facades\Schema;
+        
+        return new class extends Migration
         {
-            Schema::create('$tableName', function (Blueprint \$table) {
-                \$table->id();
-                $columns
-                \$table->timestamps();
-            });
+            public function up()
+            {
+                Schema::create('$tableName', function (Blueprint \$table) {
+                    \$table->id();
+                    $columns
+                    \$table->timestamps();
+                });
+            }
+        
+            public function down()
+            {
+                Schema::dropIfExists('$tableName');
+            }
+        };
+        EOT;
+        
+            File::put($migrationFile, $migrationContent);
         }
-    
-        public function down()
-        {
-            Schema::dropIfExists('$tableName');
-        }
-    }
-    EOT;
-    
-        File::put($migrationFile, $migrationContent);
-    }
     
 }
