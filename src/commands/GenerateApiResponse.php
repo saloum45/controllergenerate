d7121a86c5490814b4954c4847622b6a38453c13
saloum45 <saloumfall45@gmail.com>
2026-05-16 12:57:07 +0000

    in progress

--- src/commands/GenerateApiResponse.php
+++ src/commands/GenerateApiResponse.php
@@ -8,18 +8,20 @@ trait GenerateApiResponse
     protected function successResponse($data = null, $message = 'Succès', $code = 200)
     {
         return response()->json([
-            'status_code' => $code,
-            'status_message' => $message,
-            'data' => $data
+            'success' => true,
+            'message' => $message,
+            'data'    => $data,
+            'errors'  => null
         ], $code);
     }
 
     protected function errorResponse($message = 'Erreur', $code = 500, $error = null)
     {
         return response()->json([
-            'status_code' => $code,
-            'status_message' => $message,
-            'error' => $error
+            'success' => false,
+            'message' => $message,
+            'data'    => null,
+            'errors'  => $error
         ], $code);
     }
 }
